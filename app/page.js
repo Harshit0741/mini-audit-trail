474155590c38e635f37dac5251802e1e3f7a0cbe
Harshit <harshitnag12@gmail.com>
2025-11-30 20:37:22 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -19,7 +19,10 @@ export default function Home() {
 
 
   useEffect(() => {
-    fetchVersions();
+    const fetchData = async () => {
+      await fetchVersions();
+    };
+    fetchData();
   }, []);
 
   const saveVersion = async () => {
